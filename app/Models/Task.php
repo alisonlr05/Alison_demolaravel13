b724d31bd19f5c3036752b28d559d5d127a0e7cc
alisonlr05 <ALISON.LOPEZREYES@ucr.ac.cr>
2026-06-03 15:53:31 -0600

    .

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
 
-    protected $fillable = ['name', 'user_id'];
+    
 }
